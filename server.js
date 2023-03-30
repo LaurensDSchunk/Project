8ec067083429cf37510b14f75cd8d6a01f860eef
Laurens Schunk <laurensdschunk@gmail.com>
2023-03-29 20:54:28 -0600

    l

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const path = require('path');
+
 
 app.get('/', function(req, res) {
-   res.sendFile(path.resolve('/app/index.html'));
-   res.end();
+   res.sendFile(__dirname + "/app/index.html");
 });
 
 //Whenever someone connects this gets executed
